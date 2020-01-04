2927bca058b1c34afc3501b730df9200a5e4dfd3
dwinatech <diwina1993@hotmail.com>
2020-01-04 19:19:02 +0000

    Run Client app as static

--- Server/server.js
+++ Server/server.js
@@ -10,6 +10,7 @@ const api = require("./api");
 /**
  * register middleware
  */
+app.use(express.static("../Client/build"));
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -17,5 +18,12 @@ app.use(cors());
  * register routes
  */
 app.use("/api", api);
+
+// send the user to index html page inspite of the url
+if (process.env.NODE_ENV === "production") {
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../Client/build/index.html"));
+  });
+}
 
 app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`));
